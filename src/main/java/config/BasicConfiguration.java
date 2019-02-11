537714b721b4d9c4f7b8efc5c34bc5e808982508
suciumihai <36514390+suciumihai@users.noreply.github.com>
2019-02-11 21:34:06 +0200

    feb 11

--- src/main/java/config/BasicConfiguration.java
+++ src/main/java/config/BasicConfiguration.java
@@ -3,8 +3,37 @@ package config;
 import org.springframework.stereotype.Component;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+//Defines that this java bean contains configuration properties. All property names will start with basic
 @Component
 @ConfigurationProperties("basic")
 public class BasicConfiguration {
     //facem clasa asta ca sa nu ai values aiurea prin applicatie, sa fie totul centralizat
+
+    private boolean value;
+    private String message;
+    private int number;
+
+    public boolean isValue() {
+        return value;
+    }
+
+    public void setValue(boolean value) {
+        this.value = value;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
