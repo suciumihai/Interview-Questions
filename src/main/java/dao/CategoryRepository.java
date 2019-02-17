2446bcd5624e8ce3bf970810f4c66b1ee9809d4f
suciumihai <36514390+suciumihai@users.noreply.github.com>
2019-02-17 09:16:13 +0200

    renamed to dao, not repository

--- src/main/java/dao/CategoryRepository.java
+++ src/main/java/dao/CategoryRepository.java
@@ -1,4 +1,4 @@
-package repository;
+package dao;
 
 import model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
--- src/test/dao/InMemoryDBIntegrationTest.java
+++ src/test/dao/InMemoryDBIntegrationTest.java
@@ -1,4 +1,4 @@
-package repository;
+package dao;
 
 import config.JpaConfig;
 import model.Candidate;
